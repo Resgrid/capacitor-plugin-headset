805d9fc616bcb1e7768fa9ea5b57c47bdba40f09
Shawn Jackson <shawn@resgrid.com>
2022-03-06 12:58:16 -0800

    stub ios plugin

--- ios/Plugin/HeadsetPlugin.m
+++ ios/Plugin/HeadsetPlugin.m
@@ -3,6 +3,7 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(HeadsetPluginPlugin, "HeadsetPlugin",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+CAP_PLUGIN(HeadsetPlugin, "HeadsetPlugin",
+        CAP_PLUGIN_METHOD(checkPermissions, CAPPluginReturnPromise);
+        CAP_PLUGIN_METHOD(requestPermissions, CAPPluginReturnPromise);
 )
